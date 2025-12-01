d0d28c2029b726078d3f294574676ed3398b3721
Eoo0m <jseom818@gmail.com>
2025-12-01 16:17:10 +0900

    edit logic

--- main.py
+++ main.py
@@ -87,8 +87,9 @@ app.add_middleware(
         "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+    expose_headers=["*"],  # 모든 응답 헤더 노출
 )
 
 
